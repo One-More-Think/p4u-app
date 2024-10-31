5cc26c7f7dc3b7e23ea8f864dffd8171c184d791
younghoonyou <kcnoh2@hanyang.ac.kr>
2024-10-30 23:46:54 -0700

    Add encryption storage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,15 +1,21 @@
-import React, {useCallback} from 'react';
-import {View, Text, Image} from 'react-native';
-import {LoginPageStyle} from 'style';
+import React, { useCallback, useEffect } from 'react';
+import { View, Text, Image } from 'react-native';
+import { LoginPageStyle } from 'style';
 import Common from 'components/Common';
 import SocialLoginButton from 'components/SocialLoginButton';
 import store from 'reducers/index';
-import {setAuthenticated} from 'reducers/userSlice';
+import { setAuthenticated } from 'reducers/userSlice';
+import { OAuth2Login } from 'reducers/actions/UserAction';
 const LoginPage = (): React.JSX.Element => {
-  const onPress = useCallback(() => {
+  const onPress = useCallback((sns: string) => {
     // do Oauth2 and set user and authentication
-    store.dispatch(setAuthenticated(true));
+    store.dispatch(OAuth2Login(sns));
   }, []);
+
+  // useEffect(() => {
+  //   store.dispatch(OAuth2Login('google'));
+  // }, []);
+
   return (
     <Common>
       <View style={LoginPageStyle.topBodyContainer}>
@@ -21,8 +27,8 @@ const LoginPage = (): React.JSX.Element => {
         <Text style={LoginPageStyle.topBodyText}>Pick For You</Text>
       </View>
       <View style={LoginPageStyle.bottomBodyContainer}>
-        <SocialLoginButton title="Google" onPress={onPress} />
-        <SocialLoginButton title="Apple" onPress={onPress} />
+        <SocialLoginButton title="Google" onPress={() => onPress('google')} />
+        <SocialLoginButton title="Apple" onPress={() => onPress('apple')} />
       </View>
     </Common>
   );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LandingPage from 'pages/LandingPage';
 import LoginPage from 'pages/LoginPage';
 import QuestionDetailScreen from 'pages/screens/QuestionDetailScreen';
@@ -14,39 +14,40 @@ import AccountScreen from 'screens/settings/AccountScreen';
 import SearchScreen from 'screens/SearchScreen';
 import SettingPage from 'pages/SettingPage';
 import PostPage from 'pages/PostPage';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 export const MainPage = (): React.JSX.Element => {
   const MainStack = createNativeStackNavigator();
   const ScreenList = [
-    {name: 'LandingPage', component: LandingPage},
-    {name: 'QuestionDetailScreen', component: QuestionDetailScreen},
-    {name: 'NotificationScreen', component: NotificationScreen},
-    {name: 'PrivacyScreen', component: PrivacyScreen},
-    {name: 'ThemeScreen', component: ThemeScreen},
-    {name: 'LanguageScreen', component: LanguageScreen},
-    {name: 'SupportScreen', component: SupportScreen},
-    {name: 'AboutScreen', component: AboutScreen},
-    {name: 'AccountScreen', component: AccountScreen},
-    {name: 'SettingPage', component: SettingPage},
-    {name: 'SearchScreen', component: SearchScreen},
-    {name: 'PostScreen', component: PostPage},
+    { name: 'LandingPage', component: LandingPage },
+    { name: 'QuestionDetailScreen', component: QuestionDetailScreen },
+    { name: 'NotificationScreen', component: NotificationScreen },
+    { name: 'PrivacyScreen', component: PrivacyScreen },
+    { name: 'ThemeScreen', component: ThemeScreen },
+    { name: 'LanguageScreen', component: LanguageScreen },
+    { name: 'SupportScreen', component: SupportScreen },
+    { name: 'AboutScreen', component: AboutScreen },
+    { name: 'AccountScreen', component: AccountScreen },
+    { name: 'SettingPage', component: SettingPage },
+    { name: 'SearchScreen', component: SearchScreen },
+    { name: 'PostScreen', component: PostPage },
   ];
 
   const MainStackScreen = () => {
     const isAuthenticated = useSelector(
-      (state: any) => state.user.isAuthenticated,
+      (state: any) => state.user.isAuthenticated
     );
 
     return (
       <NavigationContainer>
         <MainStack.Navigator
           initialRouteName="LandingPage"
-          screenOptions={({route}) => ({
+          screenOptions={({ route }) => ({
             gestureEnabled:
               route.name === 'QuestionDetail' || route.name.includes('Screen'),
-          })}>
+          })}
+        >
           {isAuthenticated ? (
-            ScreenList.map(screen => (
+            ScreenList.map((screen) => (
               <MainStack.Screen
                 key={screen.name}
                 name={screen.name}
@@ -55,7 +56,7 @@ export const MainPage = (): React.JSX.Element => {
                   headerShown: false,
                   ...(screen.name === 'SearchScreen' ||
                   screen.name === 'PostScreen'
-                    ? {presentation: 'containedModal'}
+                    ? { presentation: 'containedModal' }
                     : {}),
                 }}
               />
@@ -65,7 +66,7 @@ export const MainPage = (): React.JSX.Element => {
               key="Login"
               name="LoginPage"
               component={LoginPage}
-              options={{headerShown: false}}
+              options={{ headerShown: false }}
             />
           )}
         </MainStack.Navigator>
